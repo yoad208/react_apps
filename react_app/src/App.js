8a5795bfa00e03a04c020a3fe4a47a484ad820cc
yoad208 <94039515+yoad208@users.noreply.github.com>
2022-06-28 00:59:58 +0300

    :100:

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,23 +1,11 @@
 import './App.css';
-import './Components/bmrCalc';
-import BmrCalc from "./Components/bmrCalc";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Component} from "react";
+import React, {Component} from "react";
 
 
 class App extends Component {
-    state = {result: null}
-
-    getResult = (_val) => {
-        this.setState({result: _val})
-    }
-
     render() {
         return (
             <div className="App">
-                <h4>BMR CALCULATOR</h4>
-                <BmrCalc getResult = {this.getResult}/>
-                <h4>{this.state.result}</h4>
             </div>
         )
     };
