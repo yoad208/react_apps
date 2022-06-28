353edf570239571cea79cee5d62b11d6295fe833
yoad208 <94039515+yoad208@users.noreply.github.com>
2022-06-28 20:21:02 +0300

    :100:

---
+++ react_app/src/Components/userInput.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class UserInput extends Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default UserInput;
