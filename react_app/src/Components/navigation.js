676af64c71826333e1a3dfc888a979c804e2e52b
yoad208 <94039515+yoad208@users.noreply.github.com>
2022-07-04 11:22:09 +0300

    :100:

--- react_app/src/Components/navigation.js
+++ react_app/src/Components/navigation.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Link} from "react-router-dom";
 
 function Navigation(props) {
 
     return (
         <Router>
-        <div className="container">
-            <div className="navbar navbar-expand-lg p-3">
+            <div className="container">
+                <div className="navbar navbar-expand-lg p-3">
                     <div className="navbar-nav">
-                        <Link className="btn-sm btn-warning text-decoration-none me-2" to="Home">Home</Link>
-                        <Link className="btn-sm btn-warning text-decoration-none me-2" to="Category's">Category's</Link>
-                        <Link className="btn-sm btn-warning text-decoration-none me-2" to="Setting">Setting</Link>
-                        <Link className="btn-sm btn-warning text-decoration-none me-2" to="Help">Help</Link>
-                        <Link className="btn-sm btn-warning text-decoration-none me-2" to="Contact">Contact</Link>
+                        <Link className="btn-sm btn-warning text-decoration-none me-2" to="/">Home</Link>
+                        <Link className="btn-sm btn-warning text-decoration-none me-2" to="/Category's">Category's</Link>
+                        <Link className="btn-sm btn-warning text-decoration-none me-2" to="/Setting">Setting</Link>
+                        <Link className="btn-sm btn-warning text-decoration-none me-2" to="/Help">Help</Link>
+                        <Link className="btn-sm btn-warning text-decoration-none me-2" to="/Contact">Contact</Link>
                     </div>
                 </div>
-        </div>
+            </div>
         </Router>
     );
 }
