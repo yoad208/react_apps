d2e6b7d6e3d73209a51d10600317c571ac482e0c
yoad208 <94039515+yoad208@users.noreply.github.com>
2022-07-21 11:22:56 +0300

    :100:

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,24 +1,13 @@
 import React, {Component} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Counter from "./Components/counter";
 
 
 class App extends Component {
-    state = {myDate: "2022-28-06", day: 0}
-    userInput = React.createRef()
-
-    changeDate = () => {
-        this.setState({myDate: this.userInput.current.value})
-    }
 
     render() {
         return (
-            <div className="App text-center border border-2 w-50 m-auto">
-                <Counter date={this.state.myDate} day={this.state.day}/>
-                <div>
-                    <input className={"text-primary h4"} ref={this.userInput} defaultValue={'Date'} type="date"/>
-                </div>
-                <button onClick={this.changeDate} className="btn btn-success">Change</button>
+            <div className="App">
+
             </div>
         )
     };
