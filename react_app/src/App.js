4e75c296a871d1f1c8e7668271b78ac4bed55947
yoad208 <94039515+yoad208@users.noreply.github.com>
2022-07-24 11:29:41 +0300

    :100:

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -5,23 +5,12 @@ import UserInput from "./Components/userInput";
 
 
 class App extends Component {
-    state = {myDate: "06-29-2022"}
 
-
-    changeDate = (_val) => {
-        this.setState({myDate: _val})// this.userInput.current.value
-    }
 
     render() {
         return (
-            <div className="App text-center row-cols-3">
+            <div className="App">
 
-                <div className="border border-dark m-auto mt-md-5 p-2">
-                    {/* get the result and puts in the screen */}
-                    <Counter date={this.state.myDate} day={this.state.day}/>
-                    {/* get the user input from other Component */}
-                    <UserInput getInput={this.changeDate}/>
-                </div>
             </div>
         )
     };
