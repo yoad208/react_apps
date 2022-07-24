521258d7c40d321e2a521eb0cf2f9831a81d89e0
yoad208 <94039515+yoad208@users.noreply.github.com>
2022-07-24 16:32:50 +0300

    :100:

--- react_app/src/Components/createList.js
+++ react_app/src/Components/createList.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faAdd} from '@fortawesome/free-solid-svg-icons'
 
 function CreateList(props) {
     return (
-        <div className="createList">
-            <div className="newList">
-                <span>Add new list</span>
-
+        <div className="createList" style={{margin: '3rem 0 0 3rem', maxWidth: '25%', textAlign: 'center'}}>
+            <div className="newList"
+                 style={{
+                     borderTop: '3px solid rgba(0,0,0,.3) ',
+                     padding: '12px',
+                     backgroundColor: 'rgba(0,0,0,.1)'
+                 }}>
+                <FontAwesomeIcon icon={faAdd}/>
+                <span style={{paddingLeft: '5px'}}>Add new list</span>
             </div>
         </div>
     );
