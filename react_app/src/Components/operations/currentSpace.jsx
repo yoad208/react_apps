5121421d82cf10c534cfbe5d6fb506ad8675561b
yoad208 <94039515+yoad208@users.noreply.github.com>
2022-08-11 13:40:24 +0300

    :100:

--- react_app/src/Components/operations/currentSpace.jsx
+++ react_app/src/Components/operations/currentSpace.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEdit, faEyeDropper, faSave, faTrashCan} from "@fortawesome/free-solid-svg-icons";
 
 export default function CurrentSpace({space}) {
     return (
-        <div>
-            {space.name}
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+            <div style={listStyle}>
+                </div>
         </div>
     );
 }
